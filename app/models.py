0a6700d600b7b9b1419d4ba340683eb81eb526eb
faithngotho <faithwangari248@gmail.com>
2019-06-25 15:04:32 +0300

    add added description part on the models.py file

--- app/models.py
+++ app/models.py
@@ -16,4 +16,11 @@ class Articles:
     '''
     Articles class to define articles objects
     '''
-    
+    def __init__(self,id,author,title,description,url,urlToImage,publishedAt):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
