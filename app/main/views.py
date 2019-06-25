72187af7df168ed352ef9013d3cf7e656c2d103a
faithngotho <faithwangari248@gmail.com>
2019-06-25 14:17:04 +0300

    add deleted statement on views.py file

--- app/main/views.py
+++ app/main/views.py
@@ -19,7 +19,5 @@ def articles(id):
 	'''
 	view articles page
 	'''
-	articles = get_articles(id)
-	title = f'NH | {id}'
-
+	
 	return render_template('articles.html',title= title, articles = articles)
